73b24f1b7b7801eadea21a7429f59ce6e173e474
liming.jiang <ar.insect@gmail.com>
2015-01-23 09:47:50 +0800

    重构中间件

--- app.js
+++ app.js
@@ -9,10 +9,12 @@ var config = require('./config');
 var routes = require('./routes');
 var _ = require('lodash');
 var ui = require('./ui');
-var cwd = process.cwd();
+var viewsWares = require('./middlewares/views');
 // application
 var app = express();
-// all environments
+// set template's path
+app.set('views', path.join(__dirname, 'views/templates'));
+// customer middleware
 app.use(express.favicon());
 //app.use(express.logger());
 app.use(express.logger('dev'));
@@ -20,29 +22,13 @@ app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(express.cookieParser('your secret here'));
 app.use(express.session());
-// customer middleware filter view
-app.use(function(req, res, next) {
-    var _render = res.render;
-    res.render = function(view, options, callback) {
-        var s = view.replace(/[\/\\]/g, '/');
-        var pre = s.substring(0,s.indexOf('/'));
-        var end = s.substring(s.indexOf('/'));
-        view = pre + '/screen' + end + '.' + config.template.extension;
-        _render.call(res, view, options, callback);
-    };
-    next();
-});
+// rewrite res.render
+app.use(viewsWares.render);
 app.use(app.router);
-// controllers
-routes(app);
-// custom middleware
-app.set('views', path.join(__dirname, 'views/templates'));
-//app.use(express.static(path.join(__dirname, 'public')));
 app.use('/public', express.static(path.join(__dirname, 'public')));
-//app.use(express.static(path.join(__dirname, 'assets')));
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
-//app.use(express.static(path.join(__dirname, 'images')));
 app.use('/images', express.static(path.join(__dirname, 'images')));
+routes( app );
 // template engine
 app.engine( config.template.extension, config.template.callback(ui.templateEngineListen) );
 // set port
@@ -51,7 +37,8 @@ var port = process.argv[2];
 port = /^\d{4,5}$/.test(port) ? port : app.get('port');
 http.createServer(app).listen(port, function() {
     console.log('Server listening on port ' + port);
-})
-    .on('error', function(err) {
-    console.log('Error',err.code);
+}).on('error', function(err) {
+    console.log('Error', err.code);
 });
+
+module.exports = app;
